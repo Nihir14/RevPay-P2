eae7d84721fcb5733936dcc5140e51ceb01f836b
Nihir Verma <nihirverma14@gmail.com>
2026-02-20 09:40:38 +0530

    bugfix-update-loan

--- src/main/java/com/revpay/controller/LoanController.java
+++ src/main/java/com/revpay/controller/LoanController.java
@@ -3,7 +3,6 @@ package com.revpay.controller;
 import com.revpay.model.dto.*;
 import com.revpay.model.entity.Loan;
 import com.revpay.model.entity.LoanInstallment;
-import com.revpay.model.dto.LoanRecommendationDTO;
 import com.revpay.service.LoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -23,91 +22,67 @@ public class LoanController {
     public LoanResponseDTO applyLoan(
             @RequestBody LoanApplyDTO dto,
             Authentication auth){
-
-        return loanService.applyLoan(
-                Long.valueOf(auth.getName()),
-                dto
-        );
+        return loanService.applyLoan(Long.valueOf(auth.getName()), dto);
     }
 
     @PostMapping("/repay")
     public String repayLoan(
             @RequestBody LoanRepayDTO dto,
             Authentication auth){
-
-        return loanService.repayLoan(
-                Long.valueOf(auth.getName()),
-                dto
-        );
+        return loanService.repayLoan(Long.valueOf(auth.getName()), dto);
     }
 
     @GetMapping("/my")
     public List<Loan> myLoans(Authentication auth){
-        return loanService.getUserLoans(
-                Long.valueOf(auth.getName())
-        );
+        return loanService.getUserLoans(Long.valueOf(auth.getName()));
     }
 
     @GetMapping("/outstanding")
     public BigDecimal totalOutstanding(Authentication auth){
-        return loanService.totalOutstanding(
-                Long.valueOf(auth.getName())
-        );
+        return loanService.totalOutstanding(Long.valueOf(auth.getName()));
     }
+
     @GetMapping("/emi/{loanId}")
     public List<LoanInstallment> viewEmiSchedule(
             @PathVariable Long loanId,
             Authentication auth){
-
-        return loanService.getEmiSchedule(
-                Long.valueOf(auth.getName()),
-                loanId
-        );
+        return loanService.getEmiSchedule(Long.valueOf(auth.getName()), loanId);
     }
+
     @GetMapping("/overdue")
     public List<LoanInstallment> getOverdues(Authentication auth){
-
-        return loanService.getOverdueEmis(
-                Long.valueOf(auth.getName())
-        );
+        return loanService.getOverdueEmis(Long.valueOf(auth.getName()));
     }
+
     @GetMapping("/analytics")
     public LoanAnalyticsDTO getAnalytics(Authentication auth){
-
         Long userId = Long.valueOf(auth.getName());
-
         return LoanAnalyticsDTO.builder()
                 .totalOutstanding(loanService.totalOutstanding(userId))
                 .totalPaid(loanService.totalPaid(userId))
                 .totalPending(loanService.totalPending(userId))
                 .build();
     }
+
     @PostMapping("/preclose/{loanId}")
     public String preCloseLoan(
             @PathVariable Long loanId,
             Authentication auth){
-
-        return loanService.preCloseLoan(
-                Long.valueOf(auth.getName()),
-                loanId
-        );
+        return loanService.preCloseLoan(Long.valueOf(auth.getName()), loanId);
     }
+
     @GetMapping("/credit-score")
     public int getCreditScore(Authentication auth){
-        return loanService.calculateCreditScore(
-                Long.valueOf(auth.getName())
-        );
+        return loanService.calculateCreditScore(Long.valueOf(auth.getName()));
     }
+
     @GetMapping("/eligibility")
     public LoanEligibilityDTO checkEligibility(Authentication auth){
-        return loanService.checkEligibility(
-                Long.valueOf(auth.getName())
-        );
+        return loanService.checkEligibility(Long.valueOf(auth.getName()));
     }
+
     @GetMapping("/recommendation")
     public LoanRecommendationDTO getRecommendation(Authentication auth){
-        return loanService.getLoanRecommendation(
-                Long.valueOf(auth.getName())
-        );
+        return loanService.getLoanRecommendation(Long.valueOf(auth.getName()));
     }
 }
