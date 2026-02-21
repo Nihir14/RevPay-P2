471b084b12d67bff5eac5affd6dcc3b43aa4568b
Rahul <rahulpatha9863@gmail.com>
2026-02-22 00:54:16 +0530

    Fixed Swagger JWT authorization issue

--- src/main/java/com/revpay/config/SwaggerConfig.java
+++ src/main/java/com/revpay/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.revpay.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +13,26 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
 
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
+
                 .info(new Info()
                         .title("RevPay API")
                         .version("1.0")
                         .description("RevPay Spring Boot Application API Documentation")
+                )
+
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName)
+                )
+
+                .schemaRequirement(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
                 );
     }
 }
-
